c99d9e49b42ddb3152dd2d43a13d1a162264c60f
super-website <kh.muhavia1@gmail.com>
2024-09-24 00:09:57 +0500

    Your descriptive commit message

---
+++ scripts/genreate-sitemap.js
@@ -0,0 +1,16 @@
+import { createSitemap } from 'sitemap'
+import fs from 'fs'
+
+// Define your routes
+const links = [
+  { url: '/', changefreq: 'daily', priority: 1.0 },
+  // Add more routes as needed
+]
+
+const sitemap = createSitemap({
+  hostname: 'https://app-factory.netlify.app/',
+  cacheTime: 600000, // 600 sec - cache purge period
+  urls: links,
+})
+
+fs.writeFileSync('./public/sitemap.xml', sitemap.toString())
