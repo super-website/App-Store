58b59057e16b21cb72b1926184720c5ec9a19efe
super-website <kh.muhavia1@gmail.com>
2024-09-24 14:53:46 +0500

    Your descriptive commit message

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,6 @@ export default {
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ['light', 'dark', 'cupcake', 'winter'],
+    themes: ['winter'],
   },
 }
