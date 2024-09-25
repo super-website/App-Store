8824f0d6694f1706bdabbb34bc1ccb8f469e83e4
super-website <kh.muhavia1@gmail.com>
2024-09-25 13:27:30 +0500

    Your descriptive commit message

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,16 @@
+import { Helmet } from 'react-helmet'
 import { About, Applications, Hero } from '../components'
-// import Slider from '../components/Slider'
 
 const Landing = () => {
   return (
     <div>
+      <Helmet>
+        <title>App Factory | Explore Our New Apps</title>
+        <meta
+          name='description'
+          content='Unlock your ideas with our app factory! We craft stunning mobile and web applications tailored to your vision, ensuring exceptional user experiences and seamless functionality.'
+        />
+      </Helmet>
       <Hero />
       <About />
       <Applications />
