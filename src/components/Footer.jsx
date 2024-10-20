ab17c53b14ae00ede7975beddf3706d6fa6bfe65
super-website <kh.muhavia1@gmail.com>
2024-10-20 22:24:20 +0500

    Your descriptive commit message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,11 +6,11 @@ const Footer = () => {
       <section id='contact-us' className='bg-slate-950'>
         <div className='mx-auto py-8 px-4 sm:px-12 xl:max-w-6xl xl:px-0 flex flex-col md:flex-row justify-between'>
           <div>
-            <h1 className='text-5xl text-left mb-6 pt-6'>
+            <h1 className='md:text-5xl text-left mb-6 pt-6 text-xl'>
               <span>Let&#39;s Talk</span>
-              <span className='text-4xl'>Do you have an idea?</span>
+              <span className='md:text-4xl text-sm'>Do you have an idea?</span>
             </h1>
-            <p className='text-left'>
+            <p className='text-left text-xs md:text-xl'>
               Contact us to learn how we can help you build the best digital
               solution for you.
             </p>
@@ -18,11 +18,13 @@ const Footer = () => {
           <div className='flex flex-col  items-end sm:mt-5 sm:items-right ml-40'>
             <a
               href='https://mail.google.com/mail/u/0/#inbox?compose=new'
-              className='btn'
+              className='btn-link'
             >
               hello@theappfactory.io
             </a>
-            <h2 className='mt-6 text-2xl sm:font-bold sm:mb-4'>Locations</h2>
+            <h2 className='mt-6 md:text-2xl sm:text-sm sm:font-bold sm:mb-4'>
+              Locations
+            </h2>
             <div className='flex flex-col items-end  mb-5 text-right'>
               <p className='text-sm '>Quezon City,PH</p>
               <span>(+63) 917-500-6605</span>
