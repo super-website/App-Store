471cf18bfaa6fa6a9a4e610e15a836899025e8c8
super-website <kh.muhavia1@gmail.com>
2025-02-27 01:21:44 +0500

    Your descriptive commit message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,44 @@
-import { useState } from 'react'
-import { pageLinks } from '../data'
-import app_logo from '../assets/app-logo.webp'
-import { Link, NavLink } from 'react-router-dom'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { useState } from "react";
+import { pageLinks } from "../data";
+import app_logo from "../assets/app-logo.webp";
+import { Link, NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
-    window.scroll(0, 0)
-    setIsOpen(false)
-  }
+    window.scroll(0, 0);
+    setIsOpen(false);
+  };
 
   const toggleMenu = () => {
-    setIsOpen((prev) => !prev)
-  }
+    setIsOpen((prev) => !prev);
+  };
 
   return (
-    <nav className='sticky top-0 inset-x-0 z-20 w-full border-b backdrop-blur border-white/30 bg-gray-900/80 py-3'>
-      <div className='mx-auto px-4 sm:px-12 xl:max-w-6xl xl:px-0'>
-        <div className='flex items-center justify-between'>
-          <div className='relative z-20'>
-            <Link to='/' onClick={handleClick}>
+    <nav className="sticky top-0 inset-x-0 z-20 w-full border-b backdrop-blur border-white/30 bg-gray-900/80 py-3">
+      <div className="mx-auto px-4 sm:px-12 xl:max-w-6xl xl:px-0">
+        <div className="flex items-center justify-between">
+          <div className="relative z-20">
+            <Link to="/" onClick={handleClick}>
               <img
                 src={app_logo}
-                className='nav-logo max-h-full max-w-full'
-                alt='App Factory'
+                className="nav-logo max-h-full max-w-full"
+                alt="App Factory"
               />
             </Link>
           </div>
           <button
-            className='lg:hidden  '
+            className="lg:hidden  "
             onClick={toggleMenu}
-            aria-label={isOpen ? 'Close menu' : 'Open menu'}
+            aria-label={isOpen ? "Close menu" : "Open menu"}
           >
             {isOpen ? <FaTimes /> : <FaBars />}
           </button>
           <ul
             className={`${
-              isOpen ? 'block' : 'hidden'
+              isOpen ? "block" : "hidden"
             } lg:flex lg:space-x-4 lg:items-center transition-all duration-300 ease-in-out absolute lg:static bg-gray-900 lg:bg-transparent w-full lg:w-auto left-0 top-full lg:top-auto lg:left-auto`}
           >
             {pageLinks.map((link) => (
@@ -48,8 +48,8 @@ const Navbar = () => {
                   onClick={handleClick}
                   className={`block px-4 py-2 transition hover:text-primary  capitalize ${
                     location.pathname === link.link
-                      ? 'text-primary font-bold'
-                      : 'text-white'
+                      ? "text-primary font-bold"
+                      : "text-white"
                   } `}
                 >
                   {link.text}
@@ -58,10 +58,10 @@ const Navbar = () => {
             ))}
             <li>
               <a
-                className='relative ml-auto flex h-9 px-4 items-center justify-center before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition-transform before:duration-300 md:hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-primaryLight max-w-xs mx-auto'
-                href='#contact-us'
+                className="relative ml-auto flex h-9 px-4 items-center justify-center before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition-transform before:duration-300 md:hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-primaryLight max-w-xs mx-auto"
+                href="#contact-us"
               >
-                <span className='relative text-base font-semibold text-gray-900 lg:text-primary lg:dark:text-white'>
+                <span className="relative text-base font-semibold   lg:dark:text-white">
                   Let's Talk
                 </span>
               </a>
@@ -70,7 +70,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
