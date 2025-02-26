431a98521e28b6bc43b7179bdf5d0dc29ab86250
super-website <kh.muhavia1@gmail.com>
2025-02-27 01:07:37 +0500

    Your descriptive commit message

--- src/pages/Application.jsx
+++ src/pages/Application.jsx
@@ -1,58 +1,58 @@
-import { apps } from '../data'
-import { Link } from 'react-router-dom'
+import { apps } from "../data";
+import { Link } from "react-router-dom";
 const Applications = () => {
   const handleClick = () => {
-    window.scroll(0, 0)
-  }
+    window.scroll(0, 0);
+  };
   return (
-    <main className='mt-10 bg-gray-900 mb-10'>
-      <div className='max-w-6xl m-auto '>
-        <div className='text-center'>
-          <h2 className='text-3xl font-semibold tracking-wide pt-12'>
+    <main className="mt-10 bg-gray-900 mb-10">
+      <div className="max-w-6xl m-auto ">
+        <div className="text-center">
+          <h2 className="text-3xl font-semibold tracking-wide pt-12">
             Meaningful digital solutions that actually work
           </h2>
         </div>
-        <div className='mt-12'>
-          <div className='p-4'>
+        <div className="mt-12">
+          <div className="p-4">
             <div
-              className='grid lg:grid-cols-3 gap-24 px-4 md:grid-cols-2 grid-cols-1 '
-              style={{ cursor: 'grab' }}
+              className="grid lg:grid-cols-3 gap-24 px-4 md:grid-cols-2 grid-cols-1 "
+              style={{ cursor: "grab" }}
             >
               {apps.map((item) => {
-                const { title, src, alt_description, description } = item
+                const { title, src, alt_description, description, slug } = item;
                 return (
                   <Link
-                    className='swiper-slide flex flex-col rounded-3xl border border-gray-100 bg-white p-6 shadow-xl shadow-gray-600/10 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none swiper-slide-active'
-                    style={{ width: '320px', marginRight: '24px' }}
+                    className="swiper-slide flex flex-col rounded-3xl border border-gray-100 bg-white p-6 shadow-xl shadow-gray-600/10 dark:border-gray-700 dark:bg-gray-800 dark:shadow-none swiper-slide-active"
+                    style={{ width: "320px", marginRight: "24px" }}
                     key={item.id}
-                    to={`/app/${item.id}`}
+                    to={`/app/${slug}`}
                     onClick={handleClick}
                   >
-                    <div className='flex h-14 w-12 items-center justify-center rounded-full bg-gradient-to-r from-primaryLight to-secondaryLight sm:w-8'>
+                    <div className="flex h-14 w-12 items-center justify-center rounded-full bg-gradient-to-r from-primaryLight to-secondaryLight sm:w-8">
                       <img
                         src={src}
                         alt={alt_description}
-                        className='max-w-full max-h-full'
+                        className="max-w-full max-h-full"
                         width={40}
                       />
                     </div>
-                    <div className='mt-4 grow'>
-                      <h3 className='text-xl font-semibold leading-8 text-gray-800 transition dark:text-white'>
+                    <div className="mt-4 grow">
+                      <h3 className="text-xl font-semibold leading-8 text-gray-800 transition dark:text-white">
                         {title}
                       </h3>
-                      <p className='mb-3 max-w-xs mt-1 text-gray-600 dark:text-gray-300'>
+                      <p className="mb-3 max-w-xs mt-1 text-gray-600 dark:text-gray-300">
                         {description}
                       </p>
                     </div>
                   </Link>
-                )
+                );
               })}
             </div>
           </div>
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Applications
+export default Applications;
--- src/pages/SinglePage.jsx
+++ src/pages/SinglePage.jsx
@@ -1,62 +1,66 @@
-import { Link, useParams } from 'react-router-dom'
-import { apps } from '../data'
-import { Helmet } from 'react-helmet'
-import { FaCheckCircle, FaApple, FaAndroid } from 'react-icons/fa'
-import Application from './Application'
+import { Link, useParams } from "react-router-dom";
+import { apps } from "../data";
+import { Helmet } from "react-helmet";
+import { FaCheckCircle, FaApple, FaAndroid } from "react-icons/fa";
+import Application from "./Application";
 
 const SinglePage = () => {
-  const { id } = useParams()
-  const page = apps.find((item) => item.id == +id)
+  const { id } = useParams();
+  const page = apps.find((item) => item.id == +id);
 
   return (
     <>
       <Helmet>
-        <title>{page.title}</title>
-        <meta name='description' content={page.alt_description} />
+        <title>{page.title} | At AppFactory</title>
+        <meta name="description" content={page.alt_description} />
+        <meta name="keywords" content={page.tags?.join(", ")} />
+        <meta property="og:title" content={page.title} />
+        <meta property="og:description" content={page.alt_description} />
       </Helmet>
-      <section className=' md:pt-0 mb-10' id='home'>
-        <div className='max-w-6xl m-auto '>
-          <div className='text-md breadcrumbs mb-3 ml-10 md:ml-0'>
+
+      <section className=" md:pt-0 mb-10" id="home">
+        <div className="max-w-6xl m-auto ">
+          <div className="text-md breadcrumbs mb-3 ml-10 md:ml-0">
             <ul>
               <li>
-                <Link to='/'>Home</Link>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to='#'>Application</Link>
+                <Link to="#">Application</Link>
               </li>
             </ul>
           </div>
-          <div className='flex flex-col md:flex-row '>
-            <div className='border-white border p-2 max-w-max md:mx-10  mx-auto md:ml-0'>
-              <img src={page.src} width='200' height='200' alt='whatsapp' />
+          <div className="flex flex-col md:flex-row ">
+            <div className="border-white border p-2 max-w-max md:mx-10  mx-auto md:ml-0">
+              <img src={page.src} width="200" height="200" alt="whatsapp" />
             </div>
-            <div className='text-left  mt-4'>
-              <div className='md:text-left text-center w-full '>
-                <h1 className='md:text-3xl text-xl text-info font-bold mb-2'>
+            <div className="text-left  mt-4">
+              <div className="md:text-left text-center w-full ">
+                <h1 className="md:text-3xl text-xl text-info font-bold mb-2">
                   {page.title}
                 </h1>
-                <p className='text-base sm:text-lg text-primary'>
+                <p className="text-base sm:text-lg text-primary">
                   {page.alt_description}
                 </p>
               </div>
 
-              <div className='mt-10 flex flex-col md:flex-row  gap-5 max-w-max md:mx-0  mx-auto '>
+              <div className="mt-10 flex flex-col md:flex-row  gap-5 max-w-max md:mx-0  mx-auto ">
                 <a
                   href={page.ios}
-                  target='blank'
-                  className='relative  flex h-11  w-44 md:w-48 items-center justify-center before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition-transform before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-primaryLight px-4 lg:before:border lg:before:border-gray-200 lg:before:bg-gray-100 lg:dark:before:bg-gray-800'
+                  target="blank"
+                  className="relative  flex h-11  w-44 md:w-48 items-center justify-center before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition-transform before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-primaryLight px-4 lg:before:border lg:before:border-gray-200 lg:before:bg-gray-100 lg:dark:before:bg-gray-800"
                 >
-                  <span className='relative md:text-base text-sm  font-semibold text-white lg:text-primary lg:dark:text-white flex items-center gap-2'>
+                  <span className="relative md:text-base text-sm  font-semibold text-white lg:text-primary lg:dark:text-white flex items-center gap-2">
                     <FaApple />
                     IOS Download
                   </span>
                 </a>
                 <a
                   href={page.android}
-                  target='blank'
-                  className='relative  flex h-11  w-44 md:w-48 items-center justify-center before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition-transform before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-primaryLight px-4 lg:before:border lg:before:border-gray-200 lg:before:bg-gray-100 lg:dark:before:bg-gray-800'
+                  target="blank"
+                  className="relative  flex h-11  w-44 md:w-48 items-center justify-center before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition-transform before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-primaryLight px-4 lg:before:border lg:before:border-gray-200 lg:before:bg-gray-100 lg:dark:before:bg-gray-800"
                 >
-                  <span className='relative md:text-base   text-sm  font-semibold text-white  lg:text-primary lg:dark:text-white flex items-center gap-2'>
+                  <span className="relative md:text-base   text-sm  font-semibold text-white  lg:text-primary lg:dark:text-white flex items-center gap-2">
                     <FaAndroid />
                     Android Download
                   </span>
@@ -66,108 +70,108 @@ const SinglePage = () => {
           </div>
         </div>
 
-        <div className='mt-28  text-center p-20  bg-white'>
-          <div className='max-w-6xl m-auto '>
-            <div className='p-5 md:p-10'>
-              <h2 className='text-2xl md:text-5xl font-bold text-black md:mb-8 mb-4 '>
+        <div className="mt-28  text-center p-20  bg-white">
+          <div className="max-w-6xl m-auto ">
+            <div className="p-5 md:p-10">
+              <h2 className="text-2xl md:text-5xl font-bold text-black md:mb-8 mb-4 ">
                 {page.title}
               </h2>
-              <p className='md:max-w-2xl m-0 md:m-auto max-w-max  text-secondary text-sm md:text-xl'>
+              <p className="md:max-w-2xl m-0 md:m-auto max-w-max  text-secondary text-sm md:text-xl">
                 {page.description}
               </p>
             </div>
           </div>
         </div>
 
-        <div className='bg-gray-100' id='about'>
-          <div className='max-w-6xl m-auto '>
-            <div className='text-center mb-10'>
-              <h2 className='text-3xl text-black font-semibold p-10'>
+        <div className="bg-gray-100" id="about">
+          <div className="max-w-6xl m-auto ">
+            <div className="text-center mb-10">
+              <h2 className="text-3xl text-black font-semibold p-10">
                 Steps & benefits
               </h2>
             </div>
 
-            <div className='grid md:grid-cols-2 gap-20 items-center mb-20 grid-cols-1'>
+            <div className="grid md:grid-cols-2 gap-20 items-center mb-20 grid-cols-1">
               <div>
                 <img
-                  src='https://licenseware.io/wp-content/uploads/2022/11/MDM-Upload-1.png'
-                  alt='upload'
-                  className='object-cover'
+                  src="https://licenseware.io/wp-content/uploads/2022/11/MDM-Upload-1.png"
+                  alt="upload"
+                  className="object-cover"
                 />
               </div>
 
-              <div className='ml-10 '>
+              <div className="ml-10 ">
                 <div>
-                  <h2 className='text-3xl text-black'>Upload</h2>
+                  <h2 className="text-3xl text-black">Upload</h2>
                 </div>
-                <div className='mt-5'>
+                <div className="mt-5">
                   <p
-                    className='flex gap-2
-                items-center mb-2 text-black'
+                    className="flex gap-2
+                items-center mb-2 text-black"
                   >
-                    <FaCheckCircle className='text-black bg-black rounded-lg' />
-                    <span className='text-black'>
+                    <FaCheckCircle className="text-black bg-black rounded-lg" />
+                    <span className="text-black">
                       Drag and drop files to upload
                     </span>
                   </p>
 
                   <p
-                    className='flex gap-4
-                items-center mb-2 text-black'
+                    className="flex gap-4
+                items-center mb-2 text-black"
                   >
-                    <FaCheckCircle className='text-black bg-black rounded-lg' />
-                    <span className='text-black'>
+                    <FaCheckCircle className="text-black bg-black rounded-lg" />
+                    <span className="text-black">
                       Data is validated automatically
                     </span>
                   </p>
 
                   <p
-                    className='flex gap-4
-                items-center mb-2 text-black'
+                    className="flex gap-4
+                items-center mb-2 text-black"
                   >
-                    <FaCheckCircle className='text-black bg-black rounded-lg' />
-                    <span className='text-black'> Click process</span>
+                    <FaCheckCircle className="text-black bg-black rounded-lg" />
+                    <span className="text-black"> Click process</span>
                   </p>
                 </div>
               </div>
             </div>
 
-            <div className='grid md:grid-cols-2 gap-20 items-center mb-20 grid-cols-1'>
-              <div className='ml-10'>
+            <div className="grid md:grid-cols-2 gap-20 items-center mb-20 grid-cols-1">
+              <div className="ml-10">
                 <div>
-                  <h2 className='text-3xl text-black'>Reports</h2>
+                  <h2 className="text-3xl text-black">Reports</h2>
                 </div>
-                <div className='mt-5'>
+                <div className="mt-5">
                   <p
-                    className='flex gap-2
-                items-center mb-2'
+                    className="flex gap-2
+                items-center mb-2"
                   >
-                    <FaCheckCircle className='text-black bg-black rounded-lg' />
+                    <FaCheckCircle className="text-black bg-black rounded-lg" />
 
-                    <span className='text-black'>
-                      {' '}
+                    <span className="text-black">
+                      {" "}
                       Identify licensing requirements
                     </span>
                   </p>
 
                   <p
-                    className='flex gap-4
-                items-center mb-2'
+                    className="flex gap-4
+                items-center mb-2"
                   >
-                    <FaCheckCircle className='text-black bg-black rounded-lg' />
+                    <FaCheckCircle className="text-black bg-black rounded-lg" />
 
-                    <span className='text-black'>
+                    <span className="text-black">
                       Understood analysis steps taken
                     </span>
                   </p>
 
                   <p
-                    className='flex gap-4
-                items-center mb-2'
+                    className="flex gap-4
+                items-center mb-2"
                   >
-                    <FaCheckCircle className='text-black bg-black rounded-lg' />
+                    <FaCheckCircle className="text-black bg-black rounded-lg" />
 
-                    <span className='text-black'>
+                    <span className="text-black">
                       Surface the required evidence
                     </span>
                   </p>
@@ -176,54 +180,54 @@ const SinglePage = () => {
 
               <div>
                 <img
-                  src='https://licenseware.io/wp-content/uploads/2022/11/MDM-Reports.png'
-                  alt='upload'
-                  className='object-cover'
+                  src="https://licenseware.io/wp-content/uploads/2022/11/MDM-Reports.png"
+                  alt="upload"
+                  className="object-cover"
                 />
               </div>
             </div>
 
-            <div className='grid md:grid-cols-2 gap-20 items-center mb-20 grid-cols-1'>
+            <div className="grid md:grid-cols-2 gap-20 items-center mb-20 grid-cols-1">
               <div>
                 <img
-                  src='https://licenseware.io/wp-content/uploads/2022/11/MDM-Data.png'
-                  alt='upload'
-                  className='object-cover'
+                  src="https://licenseware.io/wp-content/uploads/2022/11/MDM-Data.png"
+                  alt="upload"
+                  className="object-cover"
                 />
               </div>
 
-              <div className='ml-10'>
+              <div className="ml-10">
                 <div>
-                  <h2 className='text-3xl text-black'>Data Sources</h2>
+                  <h2 className="text-3xl text-black">Data Sources</h2>
                 </div>
-                <div className='mt-5'>
+                <div className="mt-5">
                   <p
-                    className='flex gap-2
-                items-center mb-2'
+                    className="flex gap-2
+                items-center mb-2"
                   >
-                    <FaCheckCircle className='text-black bg-black rounded-lg' />
-                    <span className='text-black'>
+                    <FaCheckCircle className="text-black bg-black rounded-lg" />
+                    <span className="text-black">
                       Any data source, you name it
                     </span>
                   </p>
 
                   <p
-                    className='flex gap-4
-                items-center mb-2'
+                    className="flex gap-4
+                items-center mb-2"
                   >
-                    <FaCheckCircle className='text-black bg-black rounded-lg' />
-                    <span className='text-black'>
+                    <FaCheckCircle className="text-black bg-black rounded-lg" />
+                    <span className="text-black">
                       Integration with Red Hot Discovery tool
                     </span>
                   </p>
 
                   <p
-                    className='flex gap-4
-                items-center mb-2'
+                    className="flex gap-4
+                items-center mb-2"
                   >
-                    <FaCheckCircle className='text-black bg-black rounded-lg' />
-                    <span className='text-black'>
-                      {' '}
+                    <FaCheckCircle className="text-black bg-black rounded-lg" />
+                    <span className="text-black">
+                      {" "}
                       Static upload or API integration
                     </span>
                   </p>
@@ -236,7 +240,7 @@ const SinglePage = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default SinglePage
+export default SinglePage;
